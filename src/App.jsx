4973178f54ecdb454da186d6e8f9e05944b71d5c
SoniaJ-pc <soniajaiswal@purecodemarketplace.io>
2024-02-20 12:46:10 +0530

    management-order-details

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ThemeProvider from './theme'
 import { SettingsDrawer, SettingsProvider } from './components/settings';
 import {MotionLazy} from './components/animate/motion-lazy'
 import Layout from './components/layout';
+import OrderDetailsView from './pages/order/order-details-view';
 function App() {
   return (
             <SettingsProvider
@@ -20,6 +21,7 @@ function App() {
                 <MotionLazy>
                       <SettingsDrawer />
                       <Layout>
+                    <OrderDetailsView />
                       </Layout>
                 </MotionLazy>
               </ThemeProvider>
---
+++ src/pages/order/order-details-toolbar.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+
+
+
+import { fDateTime } from '../../components/utils/format-time';
+
+import Label from '../../components/label';
+import Iconify from '../../components/iconify';
+import CustomPopover, { usePopover } from '../../components/custom-popover';
+
+// ----------------------------------------------------------------------
+
+export default function OrderDetailsToolbar({
+  status,
+  backLink,
+  createdAt,
+  orderNumber,
+  statusOptions,
+  onChangeStatus,
+}) {
+  const popover = usePopover();
+
+  return (
+    <>
+      <Stack
+        spacing={3}
+        direction={{ xs: 'column', md: 'row' }}
+        sx={{
+          mb: { xs: 3, md: 5 },
+        }}
+      >
+        <Stack spacing={1} direction="row" alignItems="flex-start">
+          <IconButton>
+            <Iconify icon="eva:arrow-ios-back-fill" />
+          </IconButton>
+
+          <Stack spacing={0.5}>
+            <Stack spacing={1} direction="row" alignItems="center">
+              <Typography variant="h4"> Order {orderNumber} </Typography>
+              <Label
+                variant="soft"
+                color={
+                  (status === 'completed' && 'success') ||
+                  (status === 'pending' && 'warning') ||
+                  (status === 'cancelled' && 'error') ||
+                  'default'
+                }
+              >
+                {status}
+              </Label>
+            </Stack>
+
+            <Typography variant="body2" sx={{ color: 'text.disabled' }}>
+              {fDateTime(createdAt)}
+            </Typography>
+          </Stack>
+        </Stack>
+
+        <Stack
+          flexGrow={1}
+          spacing={1.5}
+          direction="row"
+          alignItems="center"
+          justifyContent="flex-end"
+        >
+          <Button
+            color="inherit"
+            variant="outlined"
+            endIcon={<Iconify icon="eva:arrow-ios-downward-fill" />}
+            onClick={popover.onOpen}
+            sx={{ textTransform: 'capitalize' }}
+          >
+            {status}
+          </Button>
+
+          <Button
+            color="inherit"
+            variant="outlined"
+            startIcon={<Iconify icon="solar:printer-minimalistic-bold" />}
+          >
+            Print
+          </Button>
+
+          <Button color="inherit" variant="contained" startIcon={<Iconify icon="solar:pen-bold" />}>
+            Edit
+          </Button>
+        </Stack>
+      </Stack>
+
+      <CustomPopover
+        open={popover.open}
+        onClose={popover.onClose}
+        arrow="top-right"
+        sx={{ width: 140 }}
+      >
+        {statusOptions.map((option) => (
+          <MenuItem
+            key={option.value}
+            selected={option.value === status}
+            onClick={() => {
+              popover.onClose();
+              onChangeStatus(option.value);
+            }}
+          >
+            {option.label}
+          </MenuItem>
+        ))}
+      </CustomPopover>
+    </>
+  );
+}
