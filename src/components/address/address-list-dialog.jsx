437159aa9aa9d18c1f8694ef4b2b7f169e1a2711
SoniaJ-pc <soniajaiswal@purecodemarketplace.io>
2024-02-21 13:23:07 +0530

    management-user-account-notifications

---
+++ src/components/address/address-list-dialog.jsx
@@ -0,0 +1,152 @@
+import React from 'react';
+import { useState, useCallback } from 'react';
+
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Dialog from '@mui/material/Dialog';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import InputAdornment from '@mui/material/InputAdornment';
+import ListItemButton, { listItemButtonClasses } from '@mui/material/ListItemButton';
+
+import Label from '../label';
+import Iconify from '../iconify';
+
+
+// ----------------------------------------------------------------------
+
+export default function AddressListDialog({
+  title = 'Address Book',
+  list,
+  action,
+  //
+  open,
+  onClose,
+  //
+  selected,
+  onSelect,
+}) {
+  const [searchAddress, setSearchAddress] = useState('');
+
+  const dataFiltered = applyFilter({
+    inputData: list,
+    query: searchAddress,
+  });
+
+  const notFound = !dataFiltered.length && !!searchAddress;
+
+  const handleSearchAddress = useCallback((event) => {
+    setSearchAddress(event.target.value);
+  }, []);
+
+  const handleSelectAddress = useCallback(
+    (address) => {
+      onSelect(address);
+      setSearchAddress('');
+      onClose();
+    },
+    [onClose, onSelect]
+  );
+
+  const renderList = (
+    <Stack
+      spacing={0.5}
+      sx={{
+        p: 0.5,
+        maxHeight: 80 * 8,
+        overflowX: 'hidden',
+      }}
+    >
+      {dataFiltered.map((address) => (
+        <Stack
+          key={address.id}
+          spacing={0.5}
+          component={ListItemButton}
+          selected={selected(`${address.id}`)}
+          onClick={() => handleSelectAddress(address)}
+          sx={{
+            py: 1,
+            px: 1.5,
+            borderRadius: 1,
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            [`&.${listItemButtonClasses.selected}`]: {
+              bgcolor: 'action.selected',
+              '&:hover': {
+                bgcolor: 'action.selected',
+              },
+            },
+          }}
+        >
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <Typography variant="subtitle2">{address.name}</Typography>
+
+            {address.primary && <Label color="info">Default</Label>}
+          </Stack>
+
+          {address.company && (
+            <Box sx={{ color: 'primary.main', typography: 'caption' }}>{address.company}</Box>
+          )}
+
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            {address.fullAddress}
+          </Typography>
+
+          {address.phoneNumber && (
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+              {address.phoneNumber}
+            </Typography>
+          )}
+        </Stack>
+      ))}
+    </Stack>
+  );
+
+  return (
+    <Dialog fullWidth maxWidth="xs" open={open} onClose={onClose}>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{ p: 3, pr: 1.5 }}
+      >
+        <Typography variant="h6"> {title} </Typography>
+
+        {action && action}
+      </Stack>
+
+      <Stack sx={{ p: 2, pt: 0 }}>
+        <TextField
+          value={searchAddress}
+          onChange={handleSearchAddress}
+          placeholder="Search..."
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled' }} />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+
+      
+    </Dialog>
+  );
+}
+
+
+// ----------------------------------------------------------------------
+
+function applyFilter({ inputData, query }) {
+  if (query) {
+    return inputData.filter(
+      (address) =>
+        address.name.toLowerCase().indexOf(query.toLowerCase()) !== -1 ||
+        address.fullAddress.toLowerCase().indexOf(query.toLowerCase()) !== -1 ||
+        `${address.company}`.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    );
+  }
+
+  return inputData;
+}
---
+++ src/components/address/address-new-form.jsx
@@ -0,0 +1,137 @@
+import * as Yup from 'yup';
+import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import LoadingButton from '@mui/lab/LoadingButton';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+
+import { countries } from '../../assets/data';
+
+
+// ----------------------------------------------------------------------
+
+export default function AddressNewForm({ open, onClose, onCreate }) {
+  const NewAddressSchema = Yup.object().shape({
+    name: Yup.string().required('Fullname is required'),
+    phoneNumber: Yup.string().required('Phone number is required'),
+    address: Yup.string().required('Address is required'),
+    city: Yup.string().required('City is required'),
+    state: Yup.string().required('State is required'),
+    country: Yup.string().required('Country is required'),
+    zipCode: Yup.string().required('Zip code is required'),
+    // not required
+    addressType: Yup.string(),
+    primary: Yup.boolean(),
+  });
+
+  const defaultValues = {
+    name: '',
+    city: '',
+    state: '',
+    address: '',
+    zipCode: '',
+    primary: true,
+    phoneNumber: '',
+    addressType: 'Home',
+    country: '',
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(NewAddressSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      onCreate({
+        name: data.name,
+        phoneNumber: data.phoneNumber,
+        fullAddress: `${data.address}, ${data.city}, ${data.state}, ${data.country}, ${data.zipCode}`,
+        addressType: data.addressType,
+        primary: data.primary,
+      });
+      onClose();
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  return (
+    <Dialog fullWidth maxWidth="sm" open={open} onClose={onClose}>
+     
+        <DialogTitle>New address</DialogTitle>
+
+        <DialogContent dividers>
+          <Stack spacing={3}>
+            
+              row
+              name="addressType"
+              options={[
+                { label: 'Home', value: 'Home' },
+                { label: 'Office', value: 'Office' },
+              ]}
+           
+
+            <Box
+              rowGap={3}
+              columnGap={2}
+              display="grid"
+              gridTemplateColumns={{
+                xs: 'repeat(1, 1fr)',
+                sm: 'repeat(2, 1fr)',
+              }}
+            >
+             
+            </Box>
+
+            
+
+            <Box
+              rowGap={3}
+              columnGap={2}
+              display="grid"
+              gridTemplateColumns={{
+                xs: 'repeat(1, 1fr)',
+                sm: 'repeat(3, 1fr)',
+              }}
+            >
+              
+            </Box>
+
+           
+
+            
+          </Stack>
+        </DialogContent>
+
+        <DialogActions>
+          <Button color="inherit" variant="outlined" onClick={onClose}>
+            Cancel
+          </Button>
+
+          <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+            Deliver to this Address
+          </LoadingButton>
+        </DialogActions>
+    
+    </Dialog>
+  );
+}
+
+AddressNewForm.propTypes = {
+  onClose: PropTypes.func,
+  onCreate: PropTypes.func,
+  open: PropTypes.bool,
+};
